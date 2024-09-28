7c23ecae8595219018fe9945a08478e5f939b821
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-09-28 23:25:06 +0530

    Enhanced the Navbar buttons by adding a pop up CSS in buttons when active.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   isMenuOpen = false;
+  activeLink = '#home'; // Default active link
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -18,5 +18,9 @@ export class NavbarComponent {
 
   closeMenu() {
     this.isMenuOpen = false;
+  }
+
+  setActiveLink(link: string) {
+    this.activeLink = link;
   }
 }
