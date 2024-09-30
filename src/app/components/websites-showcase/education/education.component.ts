676b4d3cdcbb535aa2cb8668e47fc83014621ce8
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-10-01 04:04:59 +0530

    Made the EducationComponent with the templates in it.

--- src/app/components/websites-showcase/education/education.component.ts
+++ src/app/components/websites-showcase/education/education.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-education',
-  standalone: true,
-  imports: [],
   templateUrl: './education.component.html',
-  styleUrl: './education.component.scss'
+  styleUrls: ['./education.component.scss']
 })
-export class EducationComponent {
+export class EducationComponent implements AfterViewInit {
+  darkMode = false;
 
+  ngAfterViewInit(): void {
+    // GSAP animations for page heading and templates
+    gsap.from("#education-page h1", {
+      duration: 1,
+      opacity: 0,
+      y: -50,
+      ease: "power4.out"
+    });
+
+    gsap.from(".template-box", {
+      duration: 1,
+      opacity: 0,
+      scale: 0.8,
+      stagger: 0.2,
+      ease: "back.out(1.7)"
+    });
+  }
+
+  toggleTheme(): void {
+    this.darkMode = !this.darkMode;
+    const rootElement = document.documentElement;
+    if (this.darkMode) {
+      rootElement.classList.add('dark');
+    } else {
+      rootElement.classList.remove('dark');
+    }
+  }
 }
