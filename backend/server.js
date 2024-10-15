9879b27ac7378d79f15f7727248867200180427f
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-10-16 02:56:24 +0530

    Updated the server

--- backend/server.js
+++ backend/server.js
@@ -50,5 +50,5 @@ app.post('/api/verifyOtp', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on: http://localhost:${PORT}`);
 });
