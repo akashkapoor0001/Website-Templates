466b33c3d6b3189e8d8dd80fcd79cda417042180
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-10-21 14:33:22 +0530

    Updated the Shop Component

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,6 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { PhotoService } from './service/photoservice';
+// import { GalleriaModule } from 'primeng/galleria';
+
+// @Component({
+//     selector: 'app-shop',
+//     templateUrl: './shop.component.html',
+//     styleUrls: ['./shop.component.scss'],
+//     standalone: true,
+//     imports: [GalleriaModule],
+//     providers: [PhotoService]
+// })
+// export class ShopComponent implements OnInit {
+//     images: any[] | undefined;
+
+//     responsiveOptions: any[] = [
+//         {
+//             breakpoint: '1024px',
+//             numVisible: 5
+//         },
+//         {
+//             breakpoint: '768px',
+//             numVisible: 3
+//         },
+//         {
+//             breakpoint: '560px',
+//             numVisible: 1
+//         }
+//     ];
+
+//     constructor(private photoService: PhotoService) {}
+
+//     ngOnInit() {
+//         this.photoService.getImages().then((images) => (this.images = images));
+//     }
+// }
+
+
+
+
+
+
+
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PhotoService } from './service/photoservice';
-import { GalleriaModule } from 'primeng/galleria';
+import { GalleriaModule, Galleria } from 'primeng/galleria';
 
 @Component({
     selector: 'app-shop',
@@ -11,7 +54,13 @@ import { GalleriaModule } from 'primeng/galleria';
     providers: [PhotoService]
 })
 export class ShopComponent implements OnInit {
+handleItemChange($event: Event) {
+throw new Error('Method not implemented.');
+}
+    @ViewChild('galleria') galleria!: Galleria; // Correct reference for the Galleria
+
     images: any[] | undefined;
+    autoPlay = true; // Initialize autoplay as true
 
     responsiveOptions: any[] = [
         {
@@ -32,5 +81,13 @@ export class ShopComponent implements OnInit {
 
     ngOnInit() {
         this.photoService.getImages().then((images) => (this.images = images));
+    }
+
+    pauseSlideshow() {
+        this.autoPlay = false; // Pause the slideshow by disabling autoPlay
+    }
+
+    resumeSlideshow() {
+        this.autoPlay = true; // Resume the slideshow by enabling autoPlay
     }
 }
